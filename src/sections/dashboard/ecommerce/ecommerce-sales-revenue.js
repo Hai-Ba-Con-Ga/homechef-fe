bf3482427f04371e1e0b8c145ea8c105753755d1
khanhlinh.2601 <linhtkse160038@fpt.edu.vn>
2024-01-29 13:51:25 +0700

    feat: dashboard

--- src/sections/dashboard/ecommerce/ecommerce-sales-revenue.js
+++ src/sections/dashboard/ecommerce/ecommerce-sales-revenue.js
@@ -1,7 +1,10 @@
-import { Card, CardContent, CardHeader } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { format, subDays } from 'date-fns';
-import { Chart } from '../../../components/chart';
+import { Card, CardContent, CardHeader, TextField, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { format, subDays } from "date-fns";
+import { Chart } from "../../../components/chart";
+import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import { useUpdateEffect } from "@/hooks/use-update-effect";
 
 const now = new Date();
 
@@ -9,7 +12,7 @@ const createCategories = () => {
   const categories = [];
 
   for (let i = 12; i >= 0; i--) {
-    categories.push(format(subDays(now, i), 'dd MMM'));
+    categories.push(format(subDays(now, i), "dd MMM"));
   }
 
   return categories;
@@ -21,115 +24,222 @@ const useChartOptions = () => {
 
   return {
     chart: {
-      background: 'transparent',
+      background: "transparent",
       stacked: false,
       toolbar: {
-        show: false
+        show: false,
       },
       zoom: {
-        enabled: false
-      }
+        enabled: false,
+      },
     },
-    colors: [
-      theme.palette.primary.main,
-      theme.palette.warning.main
-    ],
+    colors: [theme.palette.primary.main, theme.palette.warning.main],
     dataLabels: {
-      enabled: false
+      enabled: false,
     },
     fill: {
       opacity: 1,
-      type: 'solid'
+      type: "solid",
     },
     grid: {
       borderColor: theme.palette.divider,
       strokeDashArray: 2,
       xaxis: {
         lines: {
-          show: false
-        }
+          show: false,
+        },
       },
       yaxis: {
         lines: {
-          show: true
-        }
-      }
+          show: true,
+        },
+      },
     },
     legend: {
-      horizontalAlign: 'right',
+      horizontalAlign: "right",
       labels: {
-        colors: theme.palette.text.secondary
+        colors: theme.palette.text.secondary,
       },
-      position: 'top',
-      show: true
+      position: "top",
+      show: true,
     },
     markers: {
       hover: {
         size: undefined,
-        sizeOffset: 2
+        sizeOffset: 2,
       },
       radius: 2,
-      shape: 'circle',
+      shape: "circle",
       size: 4,
-      strokeWidth: 0
+      strokeWidth: 0,
     },
     stroke: {
-      curve: 'smooth',
+      curve: "smooth",
       dashArray: [0, 3],
-      lineCap: 'butt',
-      width: 3
+      lineCap: "butt",
+      width: 3,
     },
     theme: {
-      mode: theme.palette.mode
+      mode: theme.palette.mode,
     },
     xaxis: {
       axisBorder: {
-        show: false
+        show: false,
       },
       axisTicks: {
-        show: false
+        show: false,
       },
       categories,
       labels: {
         style: {
-          colors: theme.palette.text.secondary
-        }
-      }
+          colors: theme.palette.text.secondary,
+        },
+      },
     },
     yaxis: [
       {
         axisBorder: {
-          show: false
+          show: false,
         },
         axisTicks: {
-          show: false
+          show: false,
         },
         labels: {
-          show: false
-        }
+          show: false,
+        },
       },
       {
         axisBorder: {
-          show: false
+          show: false,
         },
         axisTicks: {
-          show: false
+          show: false,
         },
         labels: {
-          show: false
-        }
-      }
-    ]
+          show: false,
+        },
+      },
+    ],
   };
 };
 
+const charts = [
+  {
+    label: "today",
+    value: {
+      chartSeries: [
+        {
+          name: "Today",
+          data: [3, 4, 7, 3, 2, 4, 7, 10],
+        },
+      ],
+    },
+  },
+  {
+    label: "week",
+    value: {
+      chartSeries: [
+        {
+          name: "Week",
+          data: [3, 4, 7, 3, 2, 4, 7, 10],
+        },
+      ],
+    },
+  },
+  {
+    label: "month",
+    value: {
+      chartSeries: [
+        {
+          name: "Month",
+          data: [3, 4, 7, 3, 2, 4, 7, 10],
+        },
+      ],
+    },
+  },
+  {
+    label: "year",
+    value: {
+      chartSeries: [
+        {
+          name: "Year",
+          data: [3, 4, 7, 3, 2, 4, 7, 10],
+        },
+      ],
+    },
+  },
+];
+
+const sortOptions = [
+  {
+    label: "Today",
+    value: "today",
+  },
+  {
+    label: "Last week",
+    value: "week",
+  },
+  {
+    label: "Last month",
+    value: "month",
+  },
+  {
+    label: "Last year",
+    value: "year",
+  },
+];
+
 export const EcommerceSalesRevenue = (props) => {
-  const { chartSeries } = props;
+  // const { chartSeries } = props;
   const chartOptions = useChartOptions();
+  const { onFiltersChange, onSortChange, sortBy } = props;
+  const [filters, setFilters] = useState({});
+  const [chartSeries, setChartSeries] = useState([]);
+
+  const handleFiltersUpdate = useCallback(() => {
+    onFiltersChange?.(filters);
+  }, [filters, onFiltersChange]);
+
+  //handle change chart
+
+  const handleSortChange = useCallback(
+    (event) => {
+      const [sortBy] = event.target.value;
+
+      onSortChange?.({ sortBy });
+    },
+    [onSortChange]
+  );
+
+  useEffect(() => {
+    console.log(sortBy);
+    const chart =
+      charts.find((item) => item.label == sortBy)?.value.chartSeries || [];
+    setChartSeries(chart);
+  }, [sortBy]);
+  useUpdateEffect(() => {
+    handleFiltersUpdate();
+  }, [filters, onFiltersChange]);
 
   return (
     <Card>
-      <CardHeader title="Orders in Week" />
+      <Stack direction="row" justifyContent="space-between" sx={{ p: 3 }}>
+        <CardHeader title="Orders" />
+        <TextField
+          label="Sort By"
+          name="sort"
+          onChange={handleSortChange}
+          select
+          SelectProps={{ native: true }}
+        >
+          {sortOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </TextField>
+      </Stack>
+
       <CardContent sx={{ pt: 0 }}>
         <Chart
           height={320}
